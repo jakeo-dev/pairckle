4e4cb15b0ad2f84c373c6fe4cfb9bc0a4cbbebac
jakeorch <jakeorchanianatjake@gmail.com>
2025-01-31 10:50:47 -0800

    add basics: choose between 2 options, generate a ranking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { Lexend } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const lexend = Lexend({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lexend.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,266 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentCombo, setCurrentCombo] = useState<number[]>([-1, -1]);
+  const [firstOption, setFirstOption] = useState<string>();
+  const [secondOption, setSecondOption] = useState<string>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [utensilInput, setUtensilInput] = useState<string>("");
+  const [utensilsArray, setUtensilsArray] = useState<
+    {
+      title: string;
+      score: number;
+    }[]
+  >([{ title: "", score: 0 }]);
+
+  const [combosArray, setCombosArray] = useState<number[][]>([[]]);
+
+  function generateCombos(array: any[]) {
+    let combinations: number[][] = [];
+
+    for (const utensil1 of array) {
+      const firstUtensil = array.indexOf(utensil1);
+
+      for (const utensil2 of array) {
+        const secondUtensil = array.indexOf(utensil2);
+
+        if (
+          /* checks if combo is the same utensil twice */
+          firstUtensil != secondUtensil &&
+          /* checks if duplicate combos in other orders already exists in combinations */
+          !combinations
+            .map((combo) => JSON.stringify(combo))
+            .includes(JSON.stringify([secondUtensil, firstUtensil]))
+        ) {
+          combinations.push([firstUtensil, secondUtensil]);
+        }
+      }
+    }
+
+    return shuffle(combinations);
+  }
+
+  // 1: 0 possible combos
+  // 2: 1 possible combo
+  // 3: 3 possible combos
+  // 4: 6 possible combos
+  // 5: 10 possible combos
+  // 6: 15 possible combos
+  // 7: 21 possible combos
+  // 8: 28 possible combo
+  // 9: 36 possible combos
+  // 10: 45 possible combos
+  // 11: 55 possible combos
+  // 12: 66 possible combos
+
+  function shuffle(array: any[]) {
+    let tempArray = array.slice();
+    let finalArray: any[] = [];
+
+    array.forEach(() => {
+      let itemNow = tempArray[Math.floor(Math.random() * tempArray.length)];
+      finalArray.push(itemNow);
+      tempArray.splice(tempArray.indexOf(itemNow), 1);
+    });
+
+    return finalArray;
+  }
+
+  function setRandomCombo(
+    combosArray: number[][],
+    utensilsArray: {
+      title: string;
+      score: number;
+    }[]
+  ) {
+    if (combosArray.length > 0) {
+      const randomCombo =
+        combosArray[Math.floor(Math.random() * combosArray.length)];
+
+      setCurrentCombo(randomCombo);
+
+      // set options to the randomly selected combo
+      setFirstOption(utensilsArray[randomCombo[0]]["title"]);
+      setSecondOption(utensilsArray[randomCombo[1]]["title"]);
+
+      // remove selected random combo so it cant be selected again
+      setCombosArray(combosArray.filter((combo) => combo !== randomCombo));
+    } else {
+      setFirstOption("");
+      setSecondOption("");
+      alert("DONE!");
+    }
+  }
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div>
+          <div>
+            <textarea
+              placeholder="Enter a list of things separated by line"
+              onChange={(e) => setUtensilInput(e.currentTarget.value)}
+              value={utensilInput}
+              className="border-2 border-gray-300 p-2"
+            ></textarea>
+            <button
+              onClick={() => {
+                if (utensilInput.trim() == "") {
+                  alert("Enter a list of things separated by line");
+                } else {
+                  let newUtensilsArray = [];
+
+                  for (const utensilTitle of utensilInput.trim().split("\n")) {
+                    newUtensilsArray.push({
+                      title: utensilTitle.trim(),
+                      score: 0,
+                    });
+                  }
+
+                  const newCombosArray = generateCombos(newUtensilsArray);
+
+                  setUtensilsArray(newUtensilsArray);
+                  setCombosArray(newCombosArray);
+                  setRandomCombo(newCombosArray, newUtensilsArray);
+                }
+              }}
+              className="bg-green-500 p-2 mb-1"
+            >
+              CLICK TO START/RESTART
+            </button>
+          </div>
+          <h1 className="text-center text-3xl my-8">Indecisive</h1>
+          <div className="flex gap-8">
+            <div className="w-[30rem] h-[14rem] text-white bg-orange-500/90 rounded-2xl flex justify-center items-center p-4 transition">
+              <h1 className="text-3xl font-semibold text-center">
+                {firstOption}
+              </h1>
+            </div>
+            <div className="w-[30rem] h-[14rem] text-white bg-blue-500/90 rounded-2xl flex justify-center items-center p-4 transition">
+              <h1 className="text-3xl font-semibold text-center">
+                {secondOption}
+              </h1>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-6 mt-8">
+            <button
+              className="w-24 h-24 rounded-full border-4 border-orange-500/70 hover:bg-orange-500/10 active:hover:bg-orange-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  // adds +3 to score of selected utensil
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[0])
+                      return { ...item, score: item.score + 3 };
+                    return item;
+                  });
+                });
+
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+3</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-orange-500/50 hover:bg-orange-500/10 active:hover:bg-orange-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[0]) {
+                      return { ...item, score: item.score + 2 };
+                    }
+                    return item;
+                  });
+                });
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+2</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-orange-500/30 hover:bg-orange-500/10 active:hover:bg-orange-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[0]) {
+                      return { ...item, score: item.score + 1 };
+                    }
+                    return item;
+                  });
+                });
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+1</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-gray-300 hover:bg-gray-200 active:bg-gray-300 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+0</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-blue-500/30 hover:bg-blue-500/10 active:hover:bg-blue-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[1]) {
+                      return { ...item, score: item.score + 1 };
+                    }
+                    return item;
+                  });
+                });
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+1</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-blue-500/50 hover:bg-blue-500/10 active:hover:bg-blue-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[1]) {
+                      return { ...item, score: item.score + 2 };
+                    }
+                    return item;
+                  });
+                });
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+2</span>
+            </button>
+            <button
+              className="w-24 h-24 rounded-full border-4 border-blue-500/70 hover:bg-blue-500/10 active:hover:bg-blue-500/30 shadow-sm hover:shadow-md active:shadow-none p-2 transition"
+              onClick={() => {
+                setUtensilsArray((prevArray) => {
+                  return prevArray.map((item, index) => {
+                    if (index === currentCombo[1]) {
+                      return { ...item, score: item.score + 3 };
+                    }
+                    return item;
+                  });
+                });
+                setRandomCombo(combosArray, utensilsArray);
+              }}
+            >
+              <span className="text-xl">+3</span>
+            </button>
+          </div>
+          <h2 className="text-center mt-4">
+            Click how much you prefer either option
+          </h2>
+          <ul>
+            {utensilsArray.map((utensil, index) => (
+              <li key={index}>
+                {utensil["title"]}: {utensil["score"]}
+              </li>
+            ))}
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
