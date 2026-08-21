b3ce9e241f17a91fed7a27b1266d49420c84246e
jakeorch <jakeaorchanian@gmail.com>
2026-08-20 18:10:16 -0700

    add privacy policy and terms of service

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function Footer({ fixed }: { fixed: boolean }) {
   return (
@@ -8,37 +7,47 @@ export default function Footer({ fixed }: { fixed: boolean }) {
         fixed ? "fixed bottom-0" : ""
       } z-10 mt-auto w-full border-t-2 border-neutral-400/15 bg-neutral-400/5 p-3 backdrop-blur-sm lg:mt-0 lg:p-4`}
     >
-      <div className="flex items-center justify-center text-center text-xs lg:text-sm">
-        <a
-          className="flex items-center transition hover:drop-shadow-md active:drop-shadow-none"
-          href="https://jakeo.dev"
-          target="_blank"
-          aria-label="Go to JakeO.dev"
-        >
-          <img
-            src="https://www.jakeo.dev/logos/bunny-jakeo-wordmark.png"
-            className="w-[3.25rem] dark:hidden lg:w-16"
-            alt="JakeO.dev logo"
-          />
-          <img
-            src="https://www.jakeo.dev/logos/bunny-jakeo-wordmark-light.png"
-            className="hidden w-[3.25rem] dark:inline lg:w-16"
-            alt="JakeO.dev logo"
-          />
-        </a>
-        <span className="mx-3 text-neutral-500 dark:text-neutral-400">•</span>
-        <a
-          className="text-neutral-800 transition hover:text-neutral-600 active:text-neutral-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:active:text-neutral-500"
-          href="https://github.com/jakeo-dev/pairckle"
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="mr-1.5 text-xs lg:text-sm"
-            aria-hidden
-          />
-          GitHub
-        </a>
+      <div className="wide-section text-center text-xs lg:text-sm">
+        <div className="flex items-center justify-center">
+          <a
+            className="-mr-2 -mt-0.5 flex items-center transition hover:drop-shadow-md active:drop-shadow-none dark:shadow-white"
+            href="https://jakeo.dev"
+            target="_blank"
+            aria-label="Go to JakeO.dev"
+          >
+            <img
+              src="https://www.jakeo.dev/logos/jakeo-wordmark.png"
+              className="w-[3.25rem] dark:hidden lg:w-[3.75rem]"
+              alt="JakeO.dev logo"
+            />
+            <img
+              src="https://www.jakeo.dev/logos/jakeo-wordmark-light.png"
+              className="hidden w-[3.25rem] dark:inline lg:w-[3.75rem]"
+              alt="JakeO.dev logo"
+            />
+          </a>
+          <span className="mx-3 text-neutral-400 dark:text-neutral-600">•</span>
+          <Link className="link" href="/privacy" target="_blank">
+            Privacy
+          </Link>
+          <span className="mx-3 text-neutral-400 dark:text-neutral-600">•</span>
+          <Link className="link" href="/terms" target="_blank">
+            Terms
+          </Link>
+          <span className="mx-3 text-neutral-400 dark:text-neutral-600">•</span>
+          <a
+            className="link"
+            href="https://github.com/jakeo-dev/pairckle"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </div>
+        {/* <div className="mt-2">
+          <p className="text-[0.675rem] text-neutral-500 md:text-xs">
+            © {new Date().getFullYear()} Pairckle
+          </p>
+        </div> */}
       </div>
     </footer>
   );
