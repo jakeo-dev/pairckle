02f12e5a70bce2b07adaf7837b444e7168b229c1
jakeo-dev <jakeaorchanian@gmail.com>
2026-09-13 15:08:29 -0700

    add option to delete account

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -10,6 +10,8 @@ export default function ConfirmModal({
   primaryButtonText,
   primaryButtonTextClicked,
   secondaryButtonText,
+  dangerousAction,
+  dangerousActionConfirmationInput,
   onConfirm,
   onCancel,
 }: {
@@ -19,12 +21,15 @@ export default function ConfirmModal({
   primaryButtonText?: string;
   primaryButtonTextClicked?: string;
   secondaryButtonText?: string;
+  dangerousAction?: boolean;
+  dangerousActionConfirmationInput?: string;
   onConfirm?: () => void;
   onCancel?: () => void;
 }) {
   const [primaryButtonTextState, setPrimaryButtonTextState] = useState<
     string | undefined
   >(primaryButtonText);
+  const [dangerousActionInput, setDangerousActionInput] = useState<string>("");
 
   return (
     <div
@@ -32,19 +37,26 @@ export default function ConfirmModal({
         visibility ? "visible-fade" : "invisible-fade"
       }`}
       onKeyDown={(e) => {
-        if (e.key === "Escape") onCancel?.();
+        if (onCancel && e.key === "Escape") {
+          setDangerousActionInput("");
+          onCancel?.();
+        }
       }}
       tabIndex={0}
     >
       <div className="relative w-[90vw] rounded-xl bg-neutral-100 px-8 py-8 shadow-md md:max-w-xl lg:p-10 dark:border-2 dark:border-neutral-800 dark:bg-black">
-        <button
-          className="absolute top-7 right-8 cursor-pointer text-lg transition hover:text-neutral-500 active:text-neutral-400 dark:active:text-neutral-600"
-          onClick={() => {
-            onCancel?.();
-          }}
-        >
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
+        {onCancel && (
+          <button
+            className="absolute top-7 right-8 cursor-pointer text-lg transition hover:text-neutral-500 active:text-neutral-400 dark:active:text-neutral-600"
+            onClick={() => {
+              setDangerousActionInput("");
+              onCancel?.();
+            }}
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        )}
+
         <h1 className="pr-4 text-lg font-medium text-pretty md:text-xl">
           {titleText}
         </h1>
@@ -53,30 +65,63 @@ export default function ConfirmModal({
             {subtitleText}
           </p>
         )}
+
+        {dangerousAction && (
+          <div className="mt-6">
+            <label
+              className="mb-0.5 block px-2 text-xs text-pretty text-black/60 lg:text-sm dark:text-white/60"
+              htmlFor="confirm-input"
+            >
+              Confirm this action
+            </label>
+            <input
+              type="text"
+              value={dangerousActionInput}
+              onChange={(e) => {
+                setDangerousActionInput(e.currentTarget.value);
+              }}
+              className="w-full rounded-lg border-2 border-neutral-400/40 bg-transparent px-3.5 py-2 text-sm outline-hidden transition placeholder:text-neutral-500/50 hover:bg-neutral-400/10 focus:bg-neutral-400/10 focus:ring-2 focus:ring-blue-300/75 active:bg-neutral-400/20 md:text-base"
+              required={true}
+              id="confirm-input"
+            />
+          </div>
+        )}
+
         <div className="float-right mt-6 flex gap-2">
-          <button
-            className={`${
-              secondaryButtonText ? "" : "hidden"
-            } cursor-pointer rounded-full border-2 border-neutral-400/40 bg-transparent px-5 py-2 text-sm transition hover:bg-neutral-400/20 active:bg-neutral-400/30 md:text-base`}
-            onClick={() => {
-              onCancel?.();
-            }}
-          >
-            {secondaryButtonText}
-          </button>
-          <button
-            className={`${
-              primaryButtonText ? "" : "hidden"
-            } cursor-pointer rounded-full bg-neutral-700/90 px-5 py-2 text-sm text-white transition hover:bg-neutral-700/80 active:bg-neutral-700/70 md:text-base dark:bg-neutral-300/90 dark:text-black dark:hover:bg-neutral-300/80 dark:active:bg-neutral-300/70`}
-            onClick={() => {
-              onConfirm?.();
-              if (primaryButtonTextClicked) {
-                setPrimaryButtonTextState(primaryButtonTextClicked);
+          {onCancel && (
+            <button
+              className="cursor-pointer rounded-full border-2 border-neutral-400/40 bg-transparent px-5 py-2 text-sm transition hover:bg-neutral-400/20 active:bg-neutral-400/30 md:text-base"
+              onClick={() => {
+                setDangerousActionInput("");
+                onCancel?.();
+              }}
+            >
+              {secondaryButtonText}
+            </button>
+          )}
+          {onConfirm && (
+            <button
+              className={`${dangerousAction && dangerousActionInput === dangerousActionConfirmationInput ? "cursor-pointer bg-red-400/30 hover:bg-red-700/80 hover:text-white active:bg-red-700/70 active:text-white dark:bg-red-400/25 dark:hover:bg-red-500/80 dark:active:bg-red-500/70" : dangerousAction ? "cursor-not-allowed bg-red-400/10 text-neutral-500/50 dark:bg-red-300/20" : "cursor-pointer bg-neutral-700/90 text-white hover:bg-neutral-700/80 active:bg-neutral-700/70 dark:bg-neutral-300/90 dark:text-black dark:hover:bg-neutral-300/80 dark:active:bg-neutral-300/70"} rounded-full px-5 py-2 text-left text-sm transition md:text-base`}
+              onClick={() => {
+                if (
+                  dangerousAction &&
+                  dangerousActionInput !== dangerousActionConfirmationInput
+                )
+                  return;
+
+                onConfirm?.();
+                if (primaryButtonTextClicked) {
+                  setPrimaryButtonTextState(primaryButtonTextClicked);
+                }
+              }}
+              disabled={
+                dangerousAction &&
+                dangerousActionInput !== dangerousActionConfirmationInput
               }
-            }}
-          >
-            {primaryButtonTextState}
-          </button>
+            >
+              {primaryButtonTextState}
+            </button>
+          )}
         </div>
       </div>
     </div>
